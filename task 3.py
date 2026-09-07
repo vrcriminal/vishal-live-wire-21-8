a1a5a86946b1ed901be612e870f5f94600e4ca69
vrcriminal <111743739+vrcriminal@users.noreply.github.com>
2026-09-07 14:25:55 +0530

    Create task 3.py

---
+++ task 3.py
@@ -0,0 +1,42 @@
+1.Take your name as input and print it in uppercase.
+2.Take a sentence and convert it into lowercase.
+3.Enter a sentence and print it with the first letter capitalized.
+4.Use format() to display your name and age in a sentence.
+5.Find the index of a given character in a word.
+6.Find the position of a substring in a sentence using find().
+7.Check if a word ends with "ing" using endswith().
+8.Print a string with tabs (\t) and then use expandtabs() to show spaces.
+9.Encode a string into UTF-8 using encode().
+10.Check if a string contains only digits using isdigit().
+11.Check if a string is numeric using isnumeric().
+12.Check if a string is alphanumeric using isalnum().
+13.Check if a string contains only ASCII characters using isascii().
+14.Check if a string contains only alphabets using isalpha().
+
+print('vishal'.upper())
+
+print('Vishal'.lower())
+
+print('vishal'.capitalize())
+
+print("My name is {} and I am {}.".format("vishal", 25))
+
+print('vishal'.index("l"))
+
+print('i am vis hal'.find("hal"))
+
+print('vishal'.endswith("ing"))
+
+print("Vvishal/".expandtabs(4))
+
+print('vishal'.encode("utf-8"))
+
+print('vishal'.isdigit())
+
+print('vishal'.isnumeric())
+
+print('vishal'.isalnum())
+
+print('vishal'.isascii())
+
+print('vishal'.isalpha())
