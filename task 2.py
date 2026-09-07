583aabb3a899516778715ee2784fafb2df0c9a3d
vrcriminal <111743739+vrcriminal@users.noreply.github.com>
2026-09-07 13:39:11 +0530

    task 2.py

--- task 2.py
+++ task 2.py
@@ -1,3 +1,4 @@
+
 #1.Whether it is positive, negative, or zero
 if a > 0 :
     print('positive')
@@ -48,3 +49,135 @@ print("Total Bill: ", bill)
 num = int(input('enter:'))
 for i in range(1, 21):
     print(num, "x", i, "=", num * i)
+
+
+#6.Get a number from the user and determine whether it is prime or not.
+num = int(input("Enter a number: "))
+factors = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        factors += 1
+if factors == 2:
+    print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")
+
+
+
+'''7.Pattern program
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+'''
+rows = 5
+
+for i in range(1, rows + 1):
+    print("* " * i)
+
+
+
+'''
+8.Pattern Program
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+'''
+rows = 5
+
+for i in range(1, rows + 1):
+    spaces = "  " * (rows - i)
+    stars = "* " * (2 * i - 1)
+    print(spaces + stars)
+
+
+
+'''
+9.Pattern Program
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+'''
+rows = 4
+
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+'''
+9.Pattern Program
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+'''
+rows = 4
+
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+
+#10. Check whether the given year is leap year or non leap year
+year = int(input("Enter a year: "))
+
+if year % 400 == 0:
+    print(year, "is a leap year.")
+elif year % 100 == 0:
+    print(year, "is a non-leap year.")
+elif year % 4 == 0:
+    print(year, "is a leap year.")
+else:
+    print(year, "is a non-leap year.")
+
+
+
+
+
+#11. Print the maximum and minimum of the given three numbers
+# Input three numbers
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+if a >= b and a >= c:
+    maximum = a
+elif b >= a and b >= c:
+    maximum = b
+else:
+    maximum = c
+
+if a <= b and a <= c:
+    minimum = a
+elif b <= a and b <= c:
+    minimum = b
+else:
+    minimum = c
+
+print("Maximum number is:", maximum)
+print("Minimum number is:", minimum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
